4b4d5c263d2efb9b9578666af819f810c8aa5ae3
Jakub Jarząbek <kubitssu@gmail.com>
2022-07-25 17:34:38 +0200

    relative paths to webdriver

--- chromeOptionDemo.py
+++ chromeOptionDemo.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+from pathlib import Path
 
 chrome_options =  webdriver.ChromeOptions()
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("headless")
 chrome_options.add_argument("--ignore-certificate-errors")
 
-service_obj = Service("/Users/kacperbiegajlo/Desktop/chromedriver/chromedriver")
+service_obj = Service(Path("./chromedriver/chromedriver").resolve())
 driver = webdriver.Chrome(service=service_obj, options=chrome_options)
 driver.implicitly_wait(2)
 
--- e2eTest.py
+++ e2eTest.py
@@ -3,8 +3,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+from pathlib import Path
 
-service_obj = Service("/Users/kacperbiegajlo/Desktop/chromedriver/chromedriver")
+service_obj = Service(Path("./chromedriver/chromedriver").resolve())
 driver = webdriver.Chrome(service=service_obj)
 driver.implicitly_wait(5)
 
